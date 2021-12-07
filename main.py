cdc354da50e063bd518dd9c0a3c6f77f11ac5798
K0D47 <94440763+K0D47@users.noreply.github.com>
2021-12-07 05:08:48 +0200

    Function deduct(number1, number2)
    Deduction of 2 parameters (the function subtacts the 2nd parameter from the 1st).

--- main.py
+++ main.py
@@ -2,3 +2,8 @@
 def add (number1, number2) :
     addition = number1 + number2
     return addition
+
+#Deduction of 2 parameters (the function subtacts the 2nd parameter from the 1st)
+def deduct (number1, number2) :
+    deduction = number1 - number2
+    return deduction
