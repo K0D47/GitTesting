6fb6878851117249c20a0e26361d18fce0c88083
K0D47 <daniel.kovacs@student.samk.fi>
2021-12-07 05:16:07 +0200

    Deduction using 2 parameters (vscode) from visual studio code: the function is defined exactly the same way. Changes deduct -> deduction.

--- main.py
+++ main.py
@@ -2,3 +2,8 @@
 def add (number1, number2) :
     addition = number1 + number2
     return addition
+
+#Deduction using 2 parameters except now from visual studio code
+def deduction (number1, number2) :
+    deduction = number1 - number2
+    return deduction
