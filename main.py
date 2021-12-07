c92dca3aac2462c203b2555950552d54c48f3f61
K0D47 <daniel.kovacs@student.samk.fi>
2021-12-07 05:29:22 +0200

    Updated the comment on function deduct() for more precision. Added "From GitHub edit:" Changed the comment from "Deduction of 2 parameters" to "Deduction using 2 parameters" Fixed an orthographic error subtacts -> subtracts

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def add (number1, number2) :
     addition = number1 + number2
     return addition
 
-#Deduction of 2 parameters (the function subtacts the 2nd parameter from the 1st)
+#From GitHub edit: Deduction using 2 parameters (the function subtracts the 2nd parameter from the 1st)
 def deduct (number1, number2) :
     deduction = number1 - number2
     return deduction
